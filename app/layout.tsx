54631bf42b95dedb536587a5992896046e1e03b6
Jack Greenberg <jackagreenberg@outlook.com>
2026-08-03 05:07:08 -0700

    Use branded site favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
     icon: [
       { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon/favicon.ico", sizes: "any" },
+      { url: "/favicon.ico", sizes: "any" },
     ],
     apple: [
       { url: "/favicon/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
